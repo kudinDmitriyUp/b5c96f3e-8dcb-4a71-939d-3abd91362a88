c0148235058078d3c73de74a287a09bb2cead556
kudinDmitriyUp <kudin.dmitriy.up@gmail.com>
2026-03-27 12:42:23 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { Figtree } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'Matcha Coffeeshop | Artisanal Ceremonial Tea',
+  description: 'Visit Matcha for premium ceremonial grade green tea, lattes, and artisanal treats in a peaceful, serene atmosphere.',
+};
+
+const figtree = Figtree({
+  variable: "--font-figtree",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${figtree.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
